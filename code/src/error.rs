ac0ae74552591c1cdbe629416e9b0a2bce5ae157
Nick Spinosa <nickspinosa1022@gmail.com>
2023-05-28 17:20:59 -0400

    inital file structure setup and adds securestore to get secret configuration

---
+++ code/src/error.rs
@@ -0,0 +1,9 @@
+
+#[derive(thiserror::Error, Debug)]
+pub enum Error {
+    #[error("Generic {0}")]
+    Generic(String),
+
+    #[error(transparent)]
+    Config(#[from] securestore::Error),
+}
--- code/src/main.rs
+++ code/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
+mod secrets;
+mod error;
+mod prelude;
+
+use secrets::secrets::get_discord_token;
+use crate::prelude::*;
+
+fn main() -> Result<()>{
     println!("Hello, world!");
+
+    println!("discord token: {}", get_discord_token()?);
+
+    Ok(())
 }
